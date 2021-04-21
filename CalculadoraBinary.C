2cb632e13eb1d7c2b36d45e5e4f754f9b4766637
Adriano de Assunção <43617865+Adrianojapa@users.noreply.github.com>
2021-04-20 23:41:35 -0300

    Update CalculadoraBinary.C

--- CalculadoraBinary.C
+++ CalculadoraBinary.C
@@ -1,21 +1,22 @@
-
+#include<stdio.h>
 #define TAM 10
-int main()
-{
-	//Conversão de decimais em binários
-
-	int numeroDecimal, binario[TAM], aux;
-
-	printf("Digite um numero decimal: ")
-	scanf("%d", &numeroDecimal);
-	
-	for(aux = TAM -1; aux >= 0; aux ==)
+int main(){
+	int decimal, binario[TAM], aux;
+	printf("1- Binario X Decimal\n")
+	printf("2- Decimal para Binário\n")
+	printf("Digite um número para opção")
+	scanf("%d", &aux);
+	if (aux == 1)
 	{
-		binario[aux]= (numeroDecimal% 2)==0 ? 0 : 1;
-		numeroDecimal = numeroDecimal / 2;
+		for (aux= TAM-1; aux>= 0; aux --){
+			binario[aux](decimal % 2) ==0 ? 0: 1;
+			decimal = decimal / 2;
+		}
+		for(aux = 0; aux > TAM; aux++){
+			printf("%d", binario[aux]);
+		}
 	}
-	printf("\n\t")
-	for(aux=0; aux < TAM; aux ++){
-		printf("%d", binario[aux]);
-	}
+	
+
+
 }
